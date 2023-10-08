59e1c91222471338d9b8abc4865965d2ede3f30b
Anna Grund <annagrund@Annas-MacBook-Pro.local>
2023-10-08 12:31:44 +0300

    g

--- vizualization_tools/model accuracy.py
+++ vizualization_tools/model accuracy.py
@@ -2,10 +2,10 @@ import plotly.express as px
 import plotly.io as pio
 
 # Number of total images
-total_images = 3889
+total_images = 2038
 
 # Number of correctly predicted images
-correct_predictions = 3190
+correct_predictions = 1848
 
 # Calculate the accuracy percentage
 accuracy_percentage = (correct_predictions / total_images) * 100

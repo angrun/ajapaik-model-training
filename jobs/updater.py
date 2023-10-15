3de25d3c03e3d2b51ffc01b9de7cd7e7e88bcf96
Anna Grund <annagrund@Annas-MacBook-Pro.local>
2023-10-15 23:24:00 +0300

    t

--- jobs/updater.py
+++ jobs/updater.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 def start():
     from .jobs import aggregate_and_retrain_model, categorize_uncategorized_images
     scheduler = BackgroundScheduler()
-    scheduler.add_job(aggregate_and_retrain_model, 'interval', minutes=5)  #TODO: review
-    scheduler.add_job(categorize_uncategorized_images, 'interval', minutes=5)
+    scheduler.add_job(aggregate_and_retrain_model, 'interval', minutes=1)  #TODO: review
+    scheduler.add_job(categorize_uncategorized_images, 'interval', minutes=1)
 
     scheduler.start()
